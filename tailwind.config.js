576034c18aad5d7a5adea74ff1b90e9200a50ba6
Eric Lewis <ericlewis777@gmail.com>
2024-02-12 15:22:40 -0500

    chore: fix tailwind warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class',
-  content: [],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
